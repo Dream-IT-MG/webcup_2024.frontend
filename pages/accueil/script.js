abf077229713e50e9c0fe9086cc652dfd4dc1b6e
Khaniee <roseiloharaoke@gmail.com>
2024-05-05 08:27:59 +0300

    accueil

--- pages/accueil/script.js
+++ pages/accueil/script.js
@@ -1,11 +1,36 @@
 import { Application } from 'https://cdn.skypack.dev/@splinetool/runtime';
 
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-
-app.load('./scene.splinecode').then(() => {
+let canvas = document.getElementById('canvas3d');
+let app = new Application(canvas);
+const godzilla = app.findObjectByName("godzilla");
+app.load('./kong.splinecode').then(() => {
     const cutie = app.findObjectByName("kong");
 
     gsap.set(cutie.scale, { x: 0.03, y: 0.03, z: 0.03 });
     gsap.set(cutie.position, { x: -100, y: -10 });
 });
+
+
+
+gsap
+    .timeline({
+        scrollTrigger: {
+            trigger: "#slide-4",
+            start: "top 60%",
+            end: "bottom bottom",
+            scrub: true,
+            onEnter: () => {
+                canvas = document.getElementById('canvasGodzilla');
+                app = new Application(canvas);
+
+                app.load('./godzilla2.splinecode').then(() => {
+
+                    gsap.set(godzilla.position, { x: 50, y: 0 });
+                });
+
+            },
+            onLeaveBack: () => {
+                document.getElementById('canvasGodzilla').style.display = "none";
+            }
+        }
+    })
